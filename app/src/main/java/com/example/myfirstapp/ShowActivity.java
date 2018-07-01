b3377740162207843336b63cc599a1843c882da6
步月随影 <1368205282@qq.com>
2018-07-01 22:37:15 +0800

    测试

--- app/src/main/java/com/example/myfirstapp/ShowActivity.java
+++ app/src/main/java/com/example/myfirstapp/ShowActivity.java
@@ -3,13 +3,20 @@ package com.example.myfirstapp;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ShowActivity extends Activity implements Button.OnClickListener{
+    public TextView tvRealTime;
     public SharedPreferences pref;
     public SharedPreferences.Editor editor;
 
@@ -17,6 +24,10 @@ public class ShowActivity extends Activity implements Button.OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+        //获取textView控件
+        tvRealTime = (TextView) findViewById(R.id.real_time);
+        new TimeThread().start();//启动线程
+
         pref = PreferenceManager.getDefaultSharedPreferences(this);
         final int flag = pref.getInt("flag", 0);
         final EditText editTeacher = (EditText)findViewById(R.id.edit_teacher);
@@ -44,11 +55,40 @@ public class ShowActivity extends Activity implements Button.OnClickListener{
                 finish();
             }
         });
-
     }
+    //写一个新的线程每隔一秒发送一次消息,这样做会和系统时间相差1秒
+    public class TimeThread extends Thread{
+        @Override
+        public void run() {
+            super.run();
+            do{
+                try {
+                    Thread.sleep(1000);
+                    Message msg = new Message();
+                    msg.what = 1;
+                    handler.sendMessage(msg);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }while (true);
+
+        }
+    }
+    private Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what){
+                case 1:
+                    tvRealTime.setText(new SimpleDateFormat("HH:mm:ss").format(new Date(System.currentTimeMillis())));
+                    break;
+            }
+            return false;
+        }
+    });
+
     @Override
     public void onClick(View v) {
 
     }
-
 }
